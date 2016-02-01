3b7ea504c709aecc2f686973f415815eb6b5cc79
kiddie235 <kiddie235@yahoo.om>
2016-02-01 18:20:12 -0500

    complete implementation of stopwatch.java file

--- Stopwatch.java
+++ Stopwatch.java
@@ -1,9 +1,32 @@
 
 public class Stopwatch {
+	private final long start; //creation time
 
-	
+	public Stopwatch() {
+		start = System.currentTimeMillis();
+	}
+
+	public double elapsedTime() {
+		long now = System.currentTimeMillis();
+		return (now - start) / 1000.0;
+	}
 
 	public static void main(String[] args) {
-		StdOut.println(price(args[0]));
+		int N = Integer.parseInt(args[0]);
+
+		double totalMath = 0.0;
+		Stopwatch swMath = new Stopwatch();
+		for (int i = 0; i < N; i++)
+			totalMath += Math.sqrt(i);
+		double timeMath = swMath.elapsedTime();
+
+		double totalNewton = 0.0;
+		Stopwatch swNewton = new Stopwatch();
+		for (int i = 0; i < N; i++)
+			totalNewton += Newton.sqrt(i);
+		double timeNewton = swNewton.elapsedTime();
+
+		StdOut.println(totalNewton/totalMath);
+		StdOut.println(timeNewton/timeMath);
 	}
 }
